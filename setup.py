af769f3b9c20abb41b6bb317bce48592bf92aef1
= <brewcrew5498@outlook.com>
2020-06-24 18:25:35 -0500

    v1.0.0

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='nlnum',
-    version='0.0.6',
+    version='1.0.0',
     author='ICLUE @ UIUC',
     author_email='',
     description='A library to compute Littlewood-Richardson coefficients and Newell-Littlewood numbers',
